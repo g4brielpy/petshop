4a69b721ed168cd001adcbc71a8d55c3f467b73f
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-09-12 07:32:24 -0300

    Versão mobile

--- src/components/HeaderTop/index.jsx
+++ src/components/HeaderTop/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export function HeaderTop() {
   return (
-    <header className="bg-slate-500">
-      <div className="container flex justify-center">
+    <header className="bg-slate-500 p-4">
+      <div className="container m-auto flex flex-col justify-center gap-4">
         <div className="flex flex-col items-center">
           <Image
             src="/cachorro-icon.png"
@@ -13,8 +14,13 @@ export function HeaderTop() {
             height="100"
             className="object-cover w-16"
           />
-          <p className="text-2xl font-bold">Melhor Amigo</p>
+          <p className="text-2xl font-bold font-sans">Melhor Amigo</p>
         </div>
+        <nav className="flex gap-8 justify-center">
+          <Link href="produtos/">Produtos</Link>
+          <Link href="galeria/">Galeria</Link>
+          <Link href="produtos/">Contatos</Link>
+        </nav>
       </div>
     </header>
   );
