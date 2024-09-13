cd9d22f62a60a0a197a9a7d61d4d871cd6b46106
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-09-13 07:51:42 -0300

    Padronizando titulos

---
+++ src/components/Titles/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Title2({ children }) {
+  return (
+    <h2 className="font-sans font-bold mb-6 text-center text-2xl md:text-3xl lg:text-4xl">
+      {children}
+    </h2>
+  );
+}
