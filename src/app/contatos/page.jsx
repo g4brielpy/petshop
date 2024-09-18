28198b84e3bf09bcbbc554eb4a4406ad6fd6d73b
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-09-18 06:51:32 -0300

    Add imports

--- src/app/contatos/page.jsx
+++ src/app/contatos/page.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+import { BannerHome } from "@/components/BannerHome";
+import {
+  TitlesBanner,
+  SubTitlesBanner,
+  TitleSection,
+  TitleCategory,
+  TitleCard,
+} from "@/components/UI/Titles";
+import { SpaceSection } from "@/components/UI/SpaceSection";
+import { Paragraph } from "@/components/UI/Paragraph";
+
 export default function contatos() {
   return (
     <>
