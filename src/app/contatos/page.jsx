e3a0c45937c23f58c2482e53911a799f2e43c856
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-09-18 21:57:38 -0300

    Formulário

--- src/app/contatos/page.jsx
+++ src/app/contatos/page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 import { BannerHome } from "@/components/BannerHome";
 import {
@@ -27,6 +28,76 @@ export default function contatos() {
           <p>Economize 20% no seu primeiro pedido</p>
         </div>
       </BannerHome>
+
+      <main className="container mx-auto p-4">
+        <SpaceSection />
+
+        <section>
+          <TitleSection>
+            Entre em <span className="text-verde2">contato</span> conosco
+          </TitleSection>
+          <div className="space-y-4">
+            <Paragraph>
+              Estamos ansiosos para conhecê-lo e ao seu melhor amigo! Visite-nos
+              no Petshop "Melhor Amigo" e descubra a diferença que fazemos na
+              vida dos animais de estimação.
+            </Paragraph>
+            <Paragraph>
+              Entre em contato conosco por telefone, e-mail ou através do nosso{" "}
+              <span className="text-verde2 font-bold">
+                formulário de contato
+              </span>{" "}
+              no nosso site. Seu pet merece o melhor, e nós estamos aqui para
+              fornecer exatamente isso. Confie no Petshop "Melhor Amigo" para
+              cuidar do seu melhor amigo como se fosse nosso próprio.
+            </Paragraph>
+          </div>
+        </section>
+
+        <SpaceSection />
+        <Image
+          src="/logo-pet.png"
+          alt="Pata de Cachorro"
+          width="80"
+          height="80"
+          className="mx-auto"
+        />
+        <div>
+          <form action="#" method="post">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="name">Nome</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Ex.: Gabriel Iuri"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Ex.: contato@gmail.com"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="tel">Telefone</label>
+              <input
+                type="tel"
+                name="tel"
+                id="tel"
+                placeholder="Ex.: 31 9 9999-9999"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="mensagem">Mensagem</label>
+              <textarea name="mensagem" id="mensagem"></textarea>
+            </div>
+          </form>
+        </div>
+      </main>
     </>
   );
 }
