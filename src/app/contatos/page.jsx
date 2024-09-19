380a9ae10cd005167b42c8457f119bfc1f2fa1a0
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-09-19 07:44:43 -0300

    Add button

--- src/app/contatos/page.jsx
+++ src/app/contatos/page.jsx
@@ -104,6 +104,18 @@ export default function contatos() {
               "
               ></textarea>
             </div>
+            <div>
+              <button
+                className="
+                  bg-verde2 text-white 
+                  px-10 py-4 rounded-lg
+                  lg:w-48 lg:text-lg xl:w-52 xl:text-xl
+                  hover:opacity-85 
+                "
+              >
+                Enviar
+              </button>
+            </div>
           </form>
         </div>
 
